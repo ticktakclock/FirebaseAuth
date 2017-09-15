4f2dc71b29872fb22037f2498a347c99dd40a514
tkyal <tkyt>
2017-09-15 20:31:44 +0900

    dev storage upload

--- app/src/main/java/com/github/ticktakclock/firebaseauth/FirebaseStorageActivity.kt
+++ app/src/main/java/com/github/ticktakclock/firebaseauth/FirebaseStorageActivity.kt
@@ -1,6 +1,11 @@
 package com.github.ticktakclock.firebaseauth
 
+import android.content.ContentValues
+import android.content.Intent
+import android.media.MediaScannerConnection
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
@@ -17,8 +22,13 @@ class FirebaseStorageActivity : AppCompatActivity() {
     lateinit var mStorage: FirebaseStorage
     lateinit var mAuth: FirebaseAuth
 
+    private val RESULT_CAMERA = 0
+
     val imageView: ImageView by bindView(R.id.activity_firebase_storage_iv)
     val downloadBtn: Button by bindView(R.id.activity_firebase_storage_download_btn)
+    val uploadBtn: Button by bindView(R.id.activity_firebase_storage_upload_btn)
+
+    var pathUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +40,38 @@ class FirebaseStorageActivity : AppCompatActivity() {
         downloadBtn.setOnClickListener { v ->
             downloadFile(mAuth.currentUser, mStorage.reference, "thumbnail.jpg")
         }
+        uploadBtn.setOnClickListener { v ->
+            startCamera()
+        }
     }
 
-    fun uploadFile(user: FirebaseUser?, ref: StorageReference, fileName: String) {
+    fun startCamera() {
+        val photoName = "photo" + ".jpg"
+        val contentValue = ContentValues()
+        contentValue.put(MediaStore.Images.Media.TITLE, photoName)
+        contentValue.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+
+        pathUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValue)
+
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, pathUri)
+        startActivityForResult(intent, RESULT_CAMERA)
+    }
+
+    fun uploadFile(user: FirebaseUser?, ref: StorageReference, fileName: String, uri: Uri) {
         user ?: return
+        val uid = user.uid
+        val fileRef = ref.child("user").child(user.uid).child(fileName)
+
+        fileRef.putFile(uri).addOnCompleteListener { task ->
+            val uritask = task.result?.downloadUrl
+            Log.d("", "upload complete :" + uritask.toString())
+        }.addOnFailureListener { exception ->
+            exception.printStackTrace()
+        }
+
+
         ref.child("thumbnail.jpg").downloadUrl.addOnSuccessListener { uri ->
             ref.child(user.uid).child("images").putFile(uri)
                     .addOnSuccessListener { taskSnapshot ->
@@ -51,5 +89,26 @@ class FirebaseStorageActivity : AppCompatActivity() {
                 .using(FirebaseImageLoader())
                 .load(fileRef)
                 .into(imageView)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RESULT_CAMERA) {
+            if (resultCode != RESULT_OK) return
+            if (data == null) return
+
+            val resultUri = data.data ?: pathUri
+
+            if (resultUri == null) return
+
+            MediaScannerConnection.scanFile(
+                    this,
+                    arrayOf(pathUri?.path),
+                    arrayOf("image/jpeg"),
+                    null
+            );
+
+            uploadFile(mAuth.currentUser, mStorage.reference, "thumbnail.jpg", resultUri)
+        }
     }
 }
